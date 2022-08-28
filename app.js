7347daace8bbe0a8b38eaa32496cfc0b7ed3fe55
lind1125 <danieljlinden@gmail.com>
2022-08-28 17:44:34 -0500

    quit game functionality and conditional for multi-letter guesses

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 // HANGMAN EXERCISE
 
-let words = ['fragrant', 'monkey', 'cherish', 'ocelot', 'pancake']
+let words = ['fragrant', 'monkey', 'cherish', 'ocelot', 'pancake'];
 
-let gameWord = words[Math.floor(Math.random() * words.length)]
+let gameWord = words[Math.floor(Math.random() * words.length)];
 
-let answerArr = []
+let answerArr = [];
 
 for (let i=0; i<gameWord.length; i++) {
-  answerArr[i] = "_"
+  answerArr[i] = "_";
 }
 
-let remainingLetters = word.length
+let remainingLetters = gameWord.length;
 
 while (remainingLetters > 0) {
-  alert(answerArr.join(' '))
+  alert(answerArr.join(' '));
+  let guess = prompt('Guess a letter or click Cancel to quit the game');
+  if (guess === null) {
+    break;
+  } else if (guess.length !== 1) {
+    alert('Please enter a single letter.');
+  } else {
+    // Updat the game state with the guess
+  }
 
 }
