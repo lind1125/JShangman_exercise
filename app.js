5938a52b9a4094d6eccd616fa42e0ef8ebab0d71
lind1125 <danieljlinden@gmail.com>
2022-08-28 17:34:02 -0500

    display gameWord and remaining letters

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ let words = ['fragrant', 'monkey', 'cherish', 'ocelot', 'pancake']
 
 let gameWord = words[Math.floor(Math.random() * words.length)]
 
-console.log(gameWord)
+let answerArr = []
+
+for (let i=0; i<gameWord.length; i++) {
+  answerArr[i] = "_"
+}
+
+let remainingLetters = word.length
+
