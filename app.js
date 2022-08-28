0cca212e5a2289d991c8deb25f126f9b8d248d11
lind1125 <danieljlinden@gmail.com>
2022-08-28 15:29:18 -0500

    added icebreaker cat question

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+const catQuestion = () => {
+  let likesCats = confirm('Do you like cats?')
+  if (likesCats) {
+    alert('You\'re a cool cat.')
+  } else {
+    alert('Yeah, that\'s fine. You\'re still cool.')
+  }
+}
+
+
 const greeting = () => {
 let yourName = prompt('What\'s your name?')
 
 alert('Hello, ' + yourName)
-break
+catQuestion()
 }
 
 greeting()
+
