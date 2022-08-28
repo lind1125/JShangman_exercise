e6a9b818c221914fee0d6a3acc722690dfe670c3
lind1125 <danieljlinden@gmail.com>
2022-08-28 18:40:44 -0500

    added limited guess functionality

--- app.js
+++ app.js
@@ -13,12 +13,13 @@ for (let i=0; i<gameWord.length; i++) {
 }
 
 let remainingLetters = gameWord.length;
+let remainingGuesses = 10;
 
 
 
 
-while (remainingLetters > 0) {
-  alert(answerArr.join(' '));
+while (remainingLetters > 0 && remainingGuesses > 0) {
+  alert(answerArr.join(' ') + `\nYou have ${remainingGuesses} guesses left.`);
   let guess = prompt('Guess a letter or click Cancel to quit the game');
   let input = guess.toLowerCase()
   if (input === null) {
@@ -26,6 +27,7 @@ while (remainingLetters > 0) {
   } else if(input.length !== 1) {
     alert('Please enter a single letter.');
   } else {
+    remainingGuesses--
     for (let j = 0; j < gameWord.length; j++) {
       if (gameWord[j] === input) {
         answerArr[j] = input;
@@ -35,5 +37,9 @@ while (remainingLetters > 0) {
   }
 }
 
-alert(answerArr.join(' '))
-alert('Good job! The answer was ' + gameWord)
+if (remainingGuesses === 0) {
+  alert('You are out of guesses. Try again.')
+} else {  
+  alert(answerArr.join(' '))
+  alert('Good job! The answer was ' + gameWord)
+}
