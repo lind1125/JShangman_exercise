e5316e388e8d2f2fdcff0f5d8e572e64dcadf67d
lind1125 <danieljlinden@gmail.com>
2022-08-28 18:21:53 -0500

    responding to game input + win condition alert

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ let words = ['fragrant', 'monkey', 'cherish', 'ocelot', 'pancake'];
 
 let gameWord = words[Math.floor(Math.random() * words.length)];
 
+setTimeout(console.log(gameWord), 2000)
+
 let answerArr = [];
 
 for (let i=0; i<gameWord.length; i++) {
@@ -15,12 +17,20 @@ let remainingLetters = gameWord.length;
 while (remainingLetters > 0) {
   alert(answerArr.join(' '));
   let guess = prompt('Guess a letter or click Cancel to quit the game');
-  if (guess === null) {
+  let input = guess.toLowerCase()
+  if (input === null) {
     break;
-  } else if (guess.length !== 1) {
+  } else if(input.length !== 1) {
     alert('Please enter a single letter.');
   } else {
-    // Updat the game state with the guess
+    for (let j = 0; j < gameWord.length; j++) {
+      if (gameWord[j] === input) {
+        answerArr[j] = input;
+        remainingLetters--;
+      }
+    }
   }
-
 }
+
+alert(answerArr.join(' '))
+alert('Good job! The answer was ' + gameWord)
