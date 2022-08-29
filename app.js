c7f1e236e32a86c15e3a88755d1e3e0631955b80
lind1125 <danieljlinden@gmail.com>
2022-08-28 19:11:17 -0500

    attempted refactor using functions

--- app.js
+++ app.js
@@ -2,44 +2,80 @@
 
 let words = ['fragrant', 'monkey', 'cherish', 'ocelot', 'pancake'];
 
-let gameWord = words[Math.floor(Math.random() * words.length)];
+// let remainingGuesses = 10;
 
-setTimeout(console.log(gameWord), 2000)
+// while (remainingLetters > 0 && remainingGuesses > 0) {
 
-let answerArr = [];
+//   let input = guess.toLowerCase()
+//   if (input === null) {
+//     break;
+//   } else if(input.length !== 1) {
+//     alert('Please enter a single letter.');
+//   } else {
+//     remainingGuesses--
 
-for (let i=0; i<gameWord.length; i++) {
-  answerArr[i] = "_";
+//     }
+//   }
+// }
+
+
+// Refactoring using functions
+const pickWord = () => {
+  // return a random word
+  return words[Math.floor(Math.random() * words.length)];
 }
 
-let remainingLetters = gameWord.length;
-let remainingGuesses = 10;
-
-
-
-
-while (remainingLetters > 0 && remainingGuesses > 0) {
-  alert(answerArr.join(' ') + `\nYou have ${remainingGuesses} guesses left.`);
-  let guess = prompt('Guess a letter or click Cancel to quit the game');
-  let input = guess.toLowerCase()
-  if (input === null) {
-    break;
-  } else if(input.length !== 1) {
-    alert('Please enter a single letter.');
-  } else {
-    remainingGuesses--
-    for (let j = 0; j < gameWord.length; j++) {
-      if (gameWord[j] === input) {
-        answerArr[j] = input;
-        remainingLetters--;
-      }
-    }
+const setupAnswerArray = (word) => {
+  // Return the answer array
+  let arr = []
+  for (let i=0; i<word.length; i++) {
+    arr[i] = "_";
   }
 }
 
-if (remainingGuesses === 0) {
-  alert('You are out of guesses. Try again.')
-} else {  
-  alert(answerArr.join(' '))
-  alert('Good job! The answer was ' + gameWord)
+const showPlayerProgress = () => {
+  // use alert to show the player their progress
+  alert(answerArr.join(' ') + `\nYou have ${remainingGuesses} guesses left.`);
 }
+
+const getGuess = () => {
+  // use prompt for a guess
+  prompt('Guess a letter or click Cancel to quit the game');
+}
+
+const updateGameState = (guess, word, answerArr) => {
+  // update answer array and return a number showing many times the guess appears in the word so remainingLetters can be updated
+  for (let j = 0; j < word.length; j++) {
+    if (gameWord[j] === guess) {
+      answerArr[j] = guess;
+      remainingLetters--;
+    }
+}
+
+const showAnswerAndCongratulatePlayer = () => {
+  //  use alert to show the answer and congratulate the player
+  if (remainingGuesses === 0) {
+    alert('You are out of guesses. Try again.')
+  } else {  
+    alert(answerArr.join(' '))
+    alert('Good job! The answer was ' + gameWord)
+  }
+}
+
+let word = pickWord();
+let answerArr = setupAnswerArray(word); 
+let remainingLetters = word.length;
+
+while (remainingLetters > 0) { 
+  showPlayerProgress(answerArr);
+  let guess = getGuess();
+  if (guess === null) {
+    break;
+  } else if (guess.length !== 1) {
+    alert("Please enter a single letter.");
+  } else {
+    let correctGuesses = updateGameState(guess, word, answerArr);
+    remainingLetters -= correctGuesses; }
+} 
+
+showAnswerAndCongratulatePlayer(answerArr);
