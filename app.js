3c42ff70d59745d0378b6cbc223f9823235d3a5b
lind1125 <danieljlinden@gmail.com>
2022-08-28 19:24:28 -0500

    refactored basic game using functions

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 // HANGMAN EXERCISE
 
-let words = ['fragrant', 'monkey', 'cherish', 'ocelot', 'pancake'];
 
 // let remainingGuesses = 10;
 
 // while (remainingLetters > 0 && remainingGuesses > 0) {
+  
+  //   let input = guess.toLowerCase()
+  //   if (input === null) {
+    //     break;
+    //   } else if(input.length !== 1) {
+      //     alert('Please enter a single letter.');
+      //   } else {
+        //     remainingGuesses--
+        
+        //     }
+        //   }
+        // }
+        
+        
+        // Refactoring using functions
+let words = ['fragrant', 'monkey', 'cherish', 'ocelot', 'pancake'];
 
-//   let input = guess.toLowerCase()
-//   if (input === null) {
-//     break;
-//   } else if(input.length !== 1) {
-//     alert('Please enter a single letter.');
-//   } else {
-//     remainingGuesses--
-
-//     }
-//   }
-// }
-
-
-// Refactoring using functions
 const pickWord = () => {
   // return a random word
   return words[Math.floor(Math.random() * words.length)];
@@ -31,38 +32,38 @@ const setupAnswerArray = (word) => {
   for (let i=0; i<word.length; i++) {
     arr[i] = "_";
   }
+  return arr
 }
 
-const showPlayerProgress = () => {
+const showPlayerProgress = (answerArr) => {
   // use alert to show the player their progress
-  alert(answerArr.join(' ') + `\nYou have ${remainingGuesses} guesses left.`);
+  alert(answerArr.join(' '));
 }
 
 const getGuess = () => {
   // use prompt for a guess
-  prompt('Guess a letter or click Cancel to quit the game');
+  return prompt('Guess a letter or click Cancel to quit the game');
 }
 
 const updateGameState = (guess, word, answerArr) => {
   // update answer array and return a number showing many times the guess appears in the word so remainingLetters can be updated
+  let letterCount = 0
   for (let j = 0; j < word.length; j++) {
-    if (gameWord[j] === guess) {
+    if (word[j] === guess) {
       answerArr[j] = guess;
-      remainingLetters--;
+      letterCount++
     }
+  }
+  return letterCount
 }
 
-const showAnswerAndCongratulatePlayer = () => {
-  //  use alert to show the answer and congratulate the player
-  if (remainingGuesses === 0) {
-    alert('You are out of guesses. Try again.')
-  } else {  
+const showAnswerAndCongratulatePlayer = (answerArr) => {
     alert(answerArr.join(' '))
-    alert('Good job! The answer was ' + gameWord)
-  }
+    alert('Good job! The answer was ' + word)
 }
 
 let word = pickWord();
+console.log(word)
 let answerArr = setupAnswerArray(word); 
 let remainingLetters = word.length;
 
